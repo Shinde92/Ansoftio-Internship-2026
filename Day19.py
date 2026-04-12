0447d5b7d57cd7f106d42b0b836892ed83b6bf2b
Shinde 92 <sshejul1137@gmail.com>
2026-04-12 14:53:43 +0530

    All Done

---
+++ Day19.py
@@ -0,0 +1,19 @@
+# Define functions (like module)
+
+def addition(a, b):
+    return a + b
+
+def subtraction(a, b):
+    return a - b
+
+
+# Main program
+if __name__ == "__main__":
+    
+    # Call functions
+    result1 = addition(10, 5)
+    result2 = subtraction(10, 5)
+
+    # Print results
+    print("Addition:", result1)
+    print("Subtraction:", result2)
